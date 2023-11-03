d3b2f7115613ffcb7905cf170f041952105fa761
David <evil.dave59@gmail.com>
2023-11-03 12:28:53 +0100

    ajout de la bonne adresse dans le fetch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://whatyearisit-backend.vercel.app/year")
+fetch("https://whatyearisit-backend-rho-roan.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     document.querySelector("#year").textContent = data.year;
