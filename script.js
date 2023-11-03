4a7cf3401fbe7d28fee01ff688d0a654d1641eb8
David <evil.dave59@gmail.com>
2023-11-03 16:25:09 +0100

    date become year in textcontent

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 fetch("https://whatyearisit-backend-rho-roan.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
-    document.querySelector("#year").textContent = data.date;
+    console.log(data);
+    document.querySelector("#year").textContent = data.year;
   });
