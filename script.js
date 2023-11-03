fb3176f12ddd31c515696014cc706addacb5616e
David <evil.dave59@gmail.com>
2023-11-03 15:31:58 +0100

    add the /year in fetch URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://whatyearisit-backend-rho-roan.vercel.app")
+fetch("https://whatyearisit-backend-rho-roan.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
     document.querySelector("#year").textContent = data.date;
