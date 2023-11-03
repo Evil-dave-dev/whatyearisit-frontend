05539e746a9143e9e13fee3b8420b9d42530f31c
David <evil.dave59@gmail.com>
2023-11-03 15:11:21 +0100

    recuperation de la vrai data qui vient du backend

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 fetch("https://whatyearisit-backend-rho-roan.vercel.app/year")
   .then((response) => response.json())
   .then((data) => {
-    document.querySelector("#year").textContent = data.year;
-    console.log("get the data from backend");
+    document.querySelector("#year").textContent = data.date;
   });
